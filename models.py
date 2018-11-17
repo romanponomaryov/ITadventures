635bcbf13abca486649528e2d4da6ceab2e277bf
Apestin-Ilya <ilyon@ukr.net>
2018-11-17 15:42:20 +0200

    Add models.py

---
+++ models.py
@@ -0,0 +1,11 @@
+class Movie:
+    def __init__(self, title, likes):
+        self.title = title
+        self.likes = likes
+
+
+class User:
+    def __init__(self, name, like_cont):
+        self.name = name
+        self.like_cont = like_cont
+
